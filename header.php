f9da53275b9389498707c525b850a2d6a7131a66
Samuel Bran <josesbran@gmail.com>
2015-08-13 11:59:15 -0600

    For IE

--- header.php
+++ header.php
@@ -19,8 +19,9 @@
           bloginfo('name'); }
       if ($paged > 1) {
          echo ' - page '. $paged; }?></title>
-
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  
   <meta name="description" content=""/>
   <meta name="keywords" content=""/>
   <meta name="owner" content=""/>
