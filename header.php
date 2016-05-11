eb9804f24c651983a280897d9551909a6e3455cb
Samuel Bran <josesbran@gmail.com>
2016-05-11 11:18:14 -0600

    Add favicon link

--- header.php
+++ header.php
@@ -22,7 +22,8 @@
 
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <meta name="viewport" content="width=device-width, initial-scale=1">
-
+  <link rel="shortcut icon" href="<?php bloginfo('template_directory'); ?>/images/favicon.png">
+  
   <meta name="description" content=""/>
   <meta name="keywords" content=""/>
   <meta name="owner" content=""/>
